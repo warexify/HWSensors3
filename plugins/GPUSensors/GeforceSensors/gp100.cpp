af65f6515222b652849698b67f84d623ea6aaaa2
SergeySlice <SergeySlice@users.noreply.github.com>
2017-09-16 00:45:11 -0300

    initial pascal support

--- plugins/GPUSensors/GeforceSensors/gp100.cpp
+++ plugins/GPUSensors/GeforceSensors/gp100.cpp
@@ -5,6 +5,9 @@
 //  Created by Natan Zalkin on 17/12/2016.
 //
 //
+/*
+ * Copyright 2017 Rhys Kidd
+ */
 
 #include "gp100.h"
 
@@ -15,6 +18,25 @@
 #include "nvd0.h"
 #include "nve0.h"
 #include "nouveau_therm.h"
+
+static int
+gp100_temp_get(struct nouveau_device *device)
+{
+  u32 tsensor = nv_rd32(device, 0x020460);
+  u32 inttemp = (tsensor & 0x0001fff8);
+
+  /* device SHADOWed */
+  if (tsensor & 0x40000000) {
+    nv_info(device, "reading temperature from SHADOWed sensor\n");
+  }
+
+  /* device valid */
+  if (tsensor & 0x20000000)
+    return (inttemp >> 8);
+  else
+    return -ENODEV;
+}
+
 
 bool gp100_identify(struct nouveau_device *device)
 {
@@ -50,7 +72,7 @@ void gp100_init(struct nouveau_device *device)
 //    device->gpio_find = nouveau_gpio_find;
 //    device->gpio_get = nouveau_gpio_get;
 //    device->gpio_sense = nvd0_gpio_sense;
-    device->temp_get = nv84_temp_get;
+  device->temp_get = gp100_temp_get; //nv84_temp_get;
 //    device->clocks_get = nve0_clock_read;
 //    //device->voltage_get = nouveau_voltage_get;
 //    device->pwm_get = gm107_fan_pwm_get;
