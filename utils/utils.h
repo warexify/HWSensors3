162afe53d583375fa9313b1754a80ba9ca80f6ba
vectorsigma <vectorsigma@users.noreply.github.com>
2019-06-06 18:00:57 -0300

    FakeSMC 3.5.2: removing REV, RBr and RPlt keys for newer models

--- utils/utils.h
+++ utils/utils.h
@@ -7,7 +7,7 @@
  *
  */
 
-inline UInt16 swap_value(UInt16 value) {
+static inline UInt16 swap_value(UInt16 value) {
 	return ((value & 0xff00) >> 8) | ((value & 0xff) << 8);
 }
 //we will use fpNM for voltages taking into account that input data in milliVolts while output in Volts
@@ -18,7 +18,7 @@ inline UInt16 encode_fp2e(UInt16 value) {
     return swap_value(value);
 }
 
-inline UInt16 encode_sp4b(UInt16 value) {
+static inline UInt16 encode_sp4b(UInt16 value) {
   UInt32 tmp = (value < 0x8000)?value:(~value);
   tmp = (tmp << 11) / 1000;
   value = (UInt16)(tmp & 0xffff);
@@ -27,28 +27,28 @@ inline UInt16 encode_sp4b(UInt16 value) {
 
 
 /*
-inline UInt16 encode_fp3d(UInt16 value) {
+static inline UInt16 encode_fp3d(UInt16 value) {
   UInt32 tmp = value;
   tmp = (tmp << 13) / 1000;
   value = (UInt16)(tmp & 0xffff);
   return swap_value(value);
 }
 
-inline UInt16 encode_fp4c(UInt16 value) {
+static inline UInt16 encode_fp4c(UInt16 value) {
     UInt32 tmp = value;
     tmp = (tmp << 12) / 1000;
     value = (UInt16)(tmp & 0xffff);
     return swap_value(value);
 }
 
-inline UInt16 encode_fp5b(UInt16 value) {
+static inline UInt16 encode_fp5b(UInt16 value) {
   UInt32 tmp = value;
   tmp = (tmp << 11) / 1000;
   value = (UInt16)(tmp & 0xffff);
   return swap_value(value);
 }
 
-inline UInt16 encode_sp5a(UInt16 value) {
+static inline UInt16 encode_sp5a(UInt16 value) {
   UInt32 tmp = (value < 0x8000)?value:(~value);
   tmp = (tmp << 10) / 1000;
   tmp = tmp & 0x7fff;
@@ -61,16 +61,16 @@ inline UInt16 encode_sp5a(UInt16 value) {
   return swap_value(value);
 }
 */
-inline UInt16 encode_fpe2(UInt16 value) {
+static inline UInt16 encode_fpe2(UInt16 value) {
 	return swap_value(value << 2);
 }
 
-inline UInt16 decode_fpe2(UInt16 value) {
+static inline UInt16 decode_fpe2(UInt16 value) {
 	return (swap_value(value) >> 2);
 }
 
 // https://stackoverflow.com/questions/8377412/ceil-function-how-can-we-implement-it-ourselves
-inline float hw_ceil(float f) {
+static inline float hw_ceil(float f) {
   unsigned input;
   memcpy(&input, &f, 4);
   int exponent = ((input >> 23) & 255) - 127;
@@ -96,7 +96,25 @@ inline int hw_round(float fl) {
   return fl < 0 ? fl - 0.5 : fl + 0.5;
 }
 
-inline bool process_sensor_entry(OSObject *object,
+static inline const char * hw_strstr(const char *str, const char *prefix) {
+  char c, sc;
+  size_t len;
+  
+  if ((c = *prefix++) != 0) {
+    len = strlen(prefix);
+    do {
+      do {
+        if ((sc = *str++) == 0) {
+          return (NULL);
+        }
+      } while (sc != c);
+    } while (strncmp(str, prefix, len) != 0);
+    str--;
+  }
+  return str;
+}
+
+static inline bool process_sensor_entry(OSObject *object,
                                  OSString **name,
                                  long *Ri,
                                  long *Rf,
